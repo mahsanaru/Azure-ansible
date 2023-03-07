8228e9bba945794ab3df077246a31c44c955205f
Mahsa MonemMoharrer <monemmahsa9@gmail.com>
2023-03-07 17:32:32 +0300

    added yaml-files

--- script.sh
+++ script.sh
@@ -9,64 +9,12 @@ kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storagec
 
 ## config metallb pool
 kubectl get configmap kube-proxy -n kube-system -o yaml | sed -e 's/strictARP: false/strictARP: true/' | kubectl apply -f - -n kube-system
-cat <<EOF | kubectl apply -f -
-apiVersion: metallb.io/v1beta1
-kind: IPAddressPool
-metadata:
-  name: first-pool
-  namespace: metallb-system
-spec:
-  addresses:
-  - 10.254.10.146/32
-EOF
+kubectl apply -f yaml-files/metallb.yaml
 
 ## add gitrepo
-apiVersion: source.toolkit.fluxcd.io/v1beta2
-kind: GitRepository
-metadata:
-  name: helmrepo
-  namespace: flux-system
-spec:
-  interval: 1m
-  ref:
-    branch: master
-  url: https://dev.azure.com/monemmahsa9/monemmahsa9/_git/monemmahsa9
-  secretRef:
-    name: azure-devops-pat
-
+kubectl apply -f yaml-files/gitrepository.yaml
 ## add helmrelease
-cat <<EOF | kubectl create -f -
-kind: HelmRelease
-metadata:
-  name: weather-app
-spec:
-  interval: 1m
-  install:
-    createNamespace: true
-    disableWait: true
-  chart:
-    spec:
-      chart: ./charts/weather-app
-      sourceRef:
-        kind: GitRepository
-        name: helmrepo
-        namespace: flux-system
-      interval: 1m
-EOF
-
-apiVersion: source.toolkit.fluxcd.io/v1beta2
-kind: GitRepository
-metadata:
-  name: helmrepo
-  namespace: flux-system
-spec:
-  gitImplementation: go-git
-  interval: 1m0s
-  ref:
-    branch: master
-  secretRef:
-    name: flux-system
-  url: https://dev.azure.com/monemmahsa9/monemmahsa9/_git/monemmahsa9
+kubectl apply -f yaml-files/helmrelease.yaml
 
 ## set ingress for gitlab & registry
 kubectl get ingress gitlab-webservice-default -o yaml | sed -e 's/gitlab.mahsa-monem.maxtld.dev/gitlab-mahsa-monem.maxtld.dev/' | kubectl apply -f - 
